69cbdcb7f5bc54414028ccafb90df84309141f19
simrandubeyy <40880251+simrandubeyy@users.noreply.github.com>
2020-10-19 16:26:28 +0530

    Update juggling.cpp

--- Juggling Algorithm/juggling.cpp
+++ Juggling Algorithm/juggling.cpp
@@ -2,21 +2,20 @@
 #define ll long long int
 using namespace std;
 
+//GCD using euclidean algorithm for Number of Sets.
 ll gcd(ll n, ll k)
 {
     if (k == 0)
         return n;
-    return gcd(k, n % k); 
-     
+    return gcd(k, n % k);     
 }
 
 int main()
 {
 	ll n,k;
-	
 	cin>>n>>k;
 	
-	ll arr[n];
+	ll *arr=new ll[n];
 	
 	for(ll i=0; i<n; i++)
 	{
@@ -25,11 +24,12 @@ int main()
 	
 	ll gcdd=gcd(n,k);
 	
+	//i to Number of Set
 	for(ll i=0; i<gcdd; i++)
 	{
 		ll j=i;
 		ll temp=arr[i];
-		
+		//break when d==i
 		while(1)
 		{	
 		  ll d=(j+k)%n;
@@ -43,6 +43,7 @@ int main()
 		  	j=d;
 		  }		
         }
+	//Update Value of j
         arr[j]=temp;
 	}
 	
